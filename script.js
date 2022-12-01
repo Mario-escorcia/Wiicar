1479394cde0b5b1b42e116df00d209cc14dfb48c
Mario-escorcia <83797431+Mario-escorcia@users.noreply.github.com>
2022-12-01 18:27:17 -0500

    lista la animacion del modal con js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const modal = document.querySelector(".modal--container");
+const cargarBtn = document.getElementById("btn--select");
 
+cargarBtn.addEventListener("click" , (e)=>{
+    if (e.target.value === "carga-masiva") {
+        modal.style.display ="inline-block"
+        modal.style.display ="flex"
+        setTimeout(()=>{
+            modal.style.display ="none"
+        },1800)
+    }
+})
