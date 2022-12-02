62c7e35db2870c171696bdb3a4d374199da224b4
Mario-escorcia <83797431+Mario-escorcia@users.noreply.github.com>
2022-12-02 10:33:33 -0500

    listo el proyecto

---
+++ README.md
@@ -0,0 +1,9 @@
+Hola , el proyecto lo desarrollé en codigo de html , css y javascript puro sin ningun tipo de libreria.
+
+la carga de los datos es simulada ya que no cuento con una API para mostrarlo , la ventana modal y otras animaciones de carga de datos se hacen conn un setTimeOut() con javascript.
+
+el proyecto cuenta conn responsive desing para hacerlo más adaptable a distintas pantallas.
+
+gracias
+
+2/12/2022
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// obtención de elemtos html con los que voy a trabajar
 const modal = document.querySelector(".modal--container");
 const cargarBtn = document.getElementById("btn--select");
 const infoContainer = document.querySelector(".info--container")
@@ -5,9 +6,10 @@ const spanInfoContainer = document.getElementById("span--info-container")
 const selectNumResultados = document.getElementById("num-resultados")
 
 
+// id de ccada elento , que irá aumentando dinamicamente en 1 en cada iteración o cada que se muestre los datos
 let idElemento = 0;
 
-// let numeroElementos = selectNumResultados.value;
+// codigo html que va a retornar el header de la tabla cuando carguen los datos
 let headerDataHtml = `<div class="table--header">
 
 <div class="input--check-cont">
@@ -60,11 +62,17 @@ let headerDataHtml = `<div class="table--header">
 </div>
 </div>`
 
+// funci´´on para cargar datos
 function cargarDatos() {
+
+    // este bucle se encarga de devolver los div html con los datos el numero de veces que tenga el valor del elemento de seleccion de valor en la parte de filtros. (por defecto empieza en 10 la carga de datos o antes de cargarlos se cambia el valor en el filtro junto al iinput de busqueda)
+
     for (let i = 0; i < selectNumResultados.value; i++) {
-                
+        
+        // creación de los elemntos html cada que se haga la iteración
         let dataHtml = document.createElement("div")
         dataHtml.classList.add("datos--container")
+        // datos internos de los div
         dataHtml.innerHTML = `<div class="input--check-datos">
         <input type="checkbox" name="" id="checkbox">
         </div>
@@ -110,8 +118,11 @@ function cargarDatos() {
             <option value="">Editar</option>
             <option value="">Almacenar</option>
         </select>
-`
+`       
+        // inserción de los datos
         infoContainer.appendChild(dataHtml)
+        
+        // aca es donde aumento de manera dinamica el id de cada elemento
         idElemento += 1;
         if (idElemento  % 2 == 0) {
             dataHtml.style.backgroundColor = "#0000001A"
@@ -120,10 +131,14 @@ function cargarDatos() {
     }
 }
 
+
+// escuchador de evento para el cargue masivo de datos
 cargarBtn.addEventListener("change" , (e)=>{
+    // si el select tiene como valor "carga masiva" en el evento change ejecuta el siguiente codigo
     if (e.target.value === "carga-masiva") {
         e.target.value = ""
        
+        // muestro la ventana modal simulando la espera al servidor tirmpo 1.8 segundos 
         modal.style.display ="inline-block"
         modal.style.display ="flex"
 
@@ -131,8 +146,12 @@ cargarBtn.addEventListener("change" , (e)=>{
             modal.style.display ="none"
             spanInfoContainer.style.display = "none"
             
+            // cargo el header de los datos
             infoContainer.innerHTML = headerDataHtml
+            // ejecuto la función que carga los div con los datos
             cargarDatos();
+
+            // actualiza los datos cada que cambio el valor del filtro de cantidad de datos
             selectNumResultados.addEventListener("change", ()=>{
                 cargarDatos()
             })
@@ -142,3 +161,5 @@ cargarBtn.addEventListener("change" , (e)=>{
     }
 
 })
+
+// 2/12/2022
